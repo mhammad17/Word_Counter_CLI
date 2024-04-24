2be5e2573d8d4e419c5607ec01fa1b115b08f944
mhammad17 <hhatori0808@gmail.com>
2024-04-25 00:03:36 +0500

    adding word counter

---
+++ wordCounter.js
@@ -0,0 +1,12 @@
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+    {
+        name: "paragraph",
+        type: "input",
+        massage: "Write a paragraph or Santance for counting words"
+    }
+]);
+let words = answer.paragraph.trim().split(" "); //trim() will remove all white spaces means "spaces"
+//split() is actually writen word convert into an array
+//and split("  ") means convert space into comma, or we can say skip.
+console.log(`your paragraph have ${words.length} words`); //.length method is count words in array
